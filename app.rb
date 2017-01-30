896aea7e86a51527e0e253a0a791f3f6ef23cc25
Skip Baney <twelvelabs@gmail.com>
2017-01-30 14:37:31 -0600

    update

--- app.rb
+++ app.rb
@@ -4,6 +4,6 @@ require 'json'
 get '/' do
   content_type :json
   [
-    { id: 1, type: 'Americano', size: '12oz', roaster: 'stumptown' }
+    { id: 1, type: 'Latte', size: '12oz', roaster: 'stumptown' }
   ].to_json
 end
